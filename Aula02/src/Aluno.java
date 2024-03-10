a8b2d90e8c248219f8f835d43e07866c2761e505
SunaUezuri <wesley.jane.santos@gmail.com>
2024-03-09 21:16:53 -0300

    Erro corrigido

--- Aula02/src/Aluno.java
+++ Aula02/src/Aluno.java
@@ -1,12 +1,11 @@
 public class Aluno {
-
     String nome;
     float cp;
     float challenge;
     float ws;
 
-    float media(){
-        return cp * 0.2f + challenge * 0.2f + ws * 0.6f;
+    float getCp(){
+        return + cp * 0.2f + challenge * 0.2f + ws * 0.6f;
 
     }
 }
--- Aula02/src/Main2.java
+++ Aula02/src/Main2.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 public class Main2 {
     public static void main(String[] args) {
         Scanner analise = new Scanner(System.in);
+        Aluno estudante = new Aluno();
 
         System.out.println("Insira seu nome aqui: ");
-        String name = analise.next();
+        estudante.nome = analise.next();
 
         System.out.println("Insira sua nota da CP");
-        float cp = analise.nextFloat();
+        estudante.cp = analise.nextFloat();
 
         System.out.println("Insira sua nota do Challenge: ");
-        float challenge = analise.nextFloat();
+        estudante.challenge = analise.nextFloat();
 
         System.out.println("Insira sua nota da GS: ");
-        float gs = analise.nextFloat();
+        estudante.ws = analise.nextFloat();
 
-        float mediaBimestre =
-        System.out.println("Sua média é: " + mediaBimestre);
+        float mediaBimestre = estudante.getCp();
+        System.out.println(estudante.nome + " sua média é: " + mediaBimestre);
 
 
 
